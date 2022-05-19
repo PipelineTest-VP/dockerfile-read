85399f8dbd5c60a8244b2cf365268b99cff9349f
Vishnu Prabhakar <vishnuprabhakar7@gmail.com>
2022-05-19 16:52:11 -0400

    Added python dependencies

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const readline = require('readline');
 
 async function main() {
     try {
+        let dockerDependencies = {
+            dependencies: []
+        };
+
         if(fs.existsSync("./Dockerfile")) {
             const fileStream = fs.createReadStream('./Dockerfile');
             const rl = readline.createInterface({
@@ -16,13 +20,78 @@ async function main() {
                     const nameAndVersion = imageNameWithVersion.split(":");
                     const imageName = nameAndVersion[0];
                     const imageVersion = nameAndVersion[1] ? nameAndVersion[1] : "latest";
-                    console.log("IMAGE_NAME: ", imageName);
-                    console.log("IMAGE_VERSION: ", imageVersion);
+                    dockerDependencies.dependencies.push({
+                        imageName,
+                        imageVersion
+                    });
                 }
             }
         }
+
+        console.log("DOCKER_DEPENDENCIES: ", dockerDependencies.dependencies);
+
+        let pythonDependencies = {
+            dependencies: []
+        };
+
+        if(fs.existsSync("./requirements.txt")) {
+            const fileStream = fs.createReadStream('./requirements.txt');
+            const rl = readline.createInterface({
+                input: fileStream,
+                crlfDelay: Infinity
+            });
+
+            for await (const line of rl) {
+                if(line.includes("==")) {
+                    const dependency = line.split("==");
+                    pythonDependencies.dependencies.push({
+                        name: dependency[0].trim(),
+                        version: dependency[1].trim(),
+                        operator: "=="
+                    });
+                } else if(line.includes(">=")) {
+                    const dependency = line.split(">=");
+                    pythonDependencies.dependencies.push({
+                        name: dependency[0].trim(),
+                        version: dependency[1].trim(),
+                        operator: ">="
+                    });
+                } else if(line.includes("<=")) {
+                    const dependency = line.split("<=");
+                    pythonDependencies.dependencies.push({
+                        name: dependency[0].trim(),
+                        version: dependency[1].trim(),
+                        operator: "<="
+                    });
+                } else if(line.includes("!=")) {
+                    const dependency = line.split("!=");
+                    pythonDependencies.dependencies.push({
+                        name: dependency[0].trim(),
+                        version: dependency[1].trim(),
+                        operator: "!="
+                    });
+                } else if(line.includes("~=")) {
+                    const dependency = line.split("~=");
+                    pythonDependencies.dependencies.push({
+                        name: dependency[0].trim(),
+                        version: dependency[1].trim(),
+                        operator: "~="
+                    });
+                } else if(!line.startsWith("#")) {
+                    if(line) {
+                        pythonDependencies.dependencies.push({
+                            name: line.trim(),
+                            version: "latest",
+                            operator: ""
+                        });
+                    }
+                }
+            }
+        }
+        console.log("PYTHON_DEPENDENCIES: ", pythonDependencies.dependencies);
         
     } catch (error) {
+        console.log("error: ", error);
     }
 }
 
